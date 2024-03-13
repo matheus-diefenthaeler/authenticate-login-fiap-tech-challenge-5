86d61c48b57f3a85718a6e4c38895ceaca2bf161
matheus-diefenthaeler <matheus.dieff@gmail.com>
2024-03-13 20:39:11 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/techchallenge5authenticatelogin/TechChallenge5AuthenticateLoginApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge5authenticatelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge5AuthenticateLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge5AuthenticateLoginApplication.class, args);
+	}
+
+}
